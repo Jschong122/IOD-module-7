cf313a7f226d920ad584fe9a099a4557c8f3dbe2
Jschong122 <cilmbv03918@gmail.com>
2024-07-04 21:02:36 +0800

    module-7

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+// COMPONENT FUNCTION NAME -> INITIALISE
+const App = () => {
+  // STATES & VARIABLES
+
+  // FUNCTIONS
+
+  //RETURN
+  return (
+    <div>
+      <p>Module 7 starts here </p>
+    </div>
+  );
+};
+export default App;
